5c58bfdc9ccc1406eda6b7facfc7af67523b4f44
cjordan7 <cosmehbvjordan@gmail.com>
2021-03-05 21:40:43 +0100

    Add comment about C11 trickery

--- src/main.swift
+++ src/main.swift
@@ -7,26 +7,34 @@
 
 import Foundation
 
-// FIXME: TODO: Consider using "MARK" For separating comments
-// FIXME: TODO: Implement better errors handling
+// TODO: Consider using "MARK" For separating comments
+// TODO: Implement better errors handling
 
 
-// FIXME: TODO: Pretty printing of assembly
+// TODO: Pretty printing of assembly
 
-// FIXME: TODO: Write tests
+// TODO: Write tests
 
 
-// FIXME: TODO: Refactor this so that the pipeline is done in
+// TODO: Refactor this so that the pipeline is done in
 // two lines of code such as:
 // var compiler = Compiler()
 // compiler.run()
+
+// TODO: According to 5.1.2.2.3 Program termination of C11-Standard
+//       available here http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1570.pdf
+// If the return type of the main function is a type compatible with int, a return from the initial call to the main function is equivalent to calling the exit function with the value returned by the main function as its argument; reaching the } that terminates the main function returns a value of 0.
+// TODO: Take into account
+// int main(void)
+// int main(int argc, char **argv)
+
 
 var test = TestCases()
 test.run()
 
 var lexer = Lexer()
 
-// FIXME: TODO: Let the user pass the file through the command line
+// TODO: Let the user pass the file through the command line
 lexer.read(filename: "/Users/cosmejordan/Documents/Programming/Swift/Projects/Swift C-Compiler/src/TestCases/C Programs/Succeed/Operations/test3.c")
 lexer.run()
 
@@ -37,5 +45,5 @@ var parser = Parser(tokens: lexerTokens)
 var parseAST = parser.parseAST()
 print(parseAST)
 
-//var codeGen = CodeGeneration(parseAST: parseAST)
-//print(codeGen.generateCode())
+// var codeGen = CodeGeneration(parseAST: parseAST)
+// print(codeGen.generateCode())
